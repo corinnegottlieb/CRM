537f7d64200dddbd98580c0462e78bcc2f4fa6e5
corinnegottleb <corinneottlieb@gmail.com>
2019-02-25 13:50:22 +0200

    homepage button

--- src/ components/app/Home.js
+++ src/ components/app/Home.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
     render() {
         return (
-            <div id="home">
+            <Link to="/clients"
+                id="home">
                 Welcome to the CRM
-            </div>
+            </Link>
         )
     }
 }
