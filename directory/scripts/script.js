c29f87fd4d721adfd2a6456722a3c90f4ef890d1
corinnegottleb <corinneottlieb@gmail.com>
2019-02-25 13:21:34 +0200

    homepage fix

--- directory/scripts/script.js
+++ directory/scripts/script.js
@@ -2,9 +2,11 @@ const data = require(`../../src/data.json`)
 const Client = require(`../../server/models/Client`)
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost:27017/CRMDB")
+mongoose.connect('mongodb://heroku_mk5rfpsk:65s0ii45d2tt7l66rm4aqdqobe@ds331735.mlab.com:31735/heroku_mk5rfpsk')
 
 data.forEach(d=> {
     let client = new Client(d)
     client.save()
 })
+
+console.log(`updated clients`)
